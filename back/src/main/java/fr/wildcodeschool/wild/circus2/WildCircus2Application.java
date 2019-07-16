b9050cd509aaffb690917270d07f2740efc319b7
Victor Todeschini <victodeschini@yahoo.fr>
2019-07-16 15:20:12 +0200

    Back initialization

---
+++ back/src/main/java/fr/wildcodeschool/wild/circus2/WildCircus2Application.java
@@ -0,0 +1,13 @@
+package fr.wildcodeschool.wild.circus2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WildCircus2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WildCircus2Application.class, args);
+	}
+
+}
