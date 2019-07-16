b7bdee9398d184189864e8e3fb1ebd5dcc068e9c
Victor Todeschini <victodeschini@yahoo.fr>
2019-07-16 13:26:38 +0200

    Troupe page + routes

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,10 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { TroupeComponent } from './troupe/troupe.component';
 import { HireComponent } from './hire/hire.component';
+
+const appRoutes: Routes = [
+  {
+    path: "",
+    component: LoginComponent,
+    pathMatch: "full"
+  },
+  {
+    path: "troupe",
+    component: TroupeComponent,
+    pathMatch: "full"
+  },
+  {
+    path: "hire",
+    component: HireComponent,
+    pathMatch: "full"
+  }
+]
 
 @NgModule({
   declarations: [
@@ -14,7 +33,10 @@ import { HireComponent } from './hire/hire.component';
     HireComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
